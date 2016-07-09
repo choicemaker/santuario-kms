9b74e7b583e73db2c53b94534b49db9203ec7a29
Rick Hall <rphall@choicemaker.com>
2016-07-09 19:34:42 -0400

    Checkpt while revising AwsKmsCredentialSet

--- src/main/java/com/choicemaker/xmlencryption/AwsKmsCredentialSet.java
+++ src/main/java/com/choicemaker/xmlencryption/AwsKmsCredentialSet.java
@@ -10,10 +10,41 @@
  */
 package com.choicemaker.xmlencryption;
 
+import java.util.Properties;
+
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.choicemaker.utilcopy01.Precondition;
+import com.choicemaker.utilcopy01.StringUtils;
 
 public class AwsKmsCredentialSet extends CredentialSet {
+
+	/**
+	 * Creates an instance of AWSCredentials from
+	 * {@link AwsKmsProperties#PN_ACCESSKEY access-} and
+	 * {@link AwsKmsProperties#PN_SECRETKEY secret-}key properties defined
+	 * by the specified set of properties.
+	 * @param a non-null, non-blank set of properties
+	 * @return a non-null instance of AWSCredentials
+	 * @throws IllegalArgumentException if {@link AwsKmsProperties#PN_ACCESSKEY access-} and
+	 * {@link AwsKmsProperties#PN_SECRETKEY secret-}key properties are not defined
+	 * in the specified set of properties.
+	 */
+	public static AWSCredentials createAWSCredentials(Properties p) {
+		Precondition.assertNonNullArgument("null properties", p);
+		String accessKey = AwsKmsProperties.getAccessKey(p);
+		if (!StringUtils.nonEmptyString(accessKey)) {
+			String msg = "Missing property '" + AwsKmsProperties.PN_ACCESSKEY + "'";
+			throw new IllegalArgumentException(msg);
+		}
+		String secretKey = AwsKmsProperties.getSecretKey(p);
+		if (!StringUtils.nonEmptyString(secretKey)) {
+			String msg = "Missing property '" + AwsKmsProperties.PN_SECRETKEY + "'";
+			throw new IllegalArgumentException(msg);
+		}
+		AWSCredentials retVal = new BasicAWSCredentials(accessKey, secretKey);
+		return retVal;
+	}
 
 	/**
 	 * Creates an invalid credential set with the specified name. To convert
@@ -28,8 +59,16 @@ public class AwsKmsCredentialSet extends CredentialSet {
 		super(name);
 	}
 
+	/**
+	 * Creates a valid credential set from the specified properties.
+	 * 
+	 * @param name
+	 * @param
+	 * @param masterKeyId
+	 * @param endpoint
+	 */
 	public AwsKmsCredentialSet(String name, String masterKeyId, String endpoint) {
-		this(AwsKmsUtils.getDefaultAWSCredentials(), name, masterKeyId,
+		this(AwsKmsProperties.getDefaultAWSCredentials(), name, masterKeyId,
 				endpoint);
 	}
 
