acab4a7129295f99613d20c8ad83e5e6639cc97a
Rick Hall <rphall@choicemaker.com>
2016-12-23 11:55:43 -0500

    Working method for StreamEncryptor.encrypt(..)

--- src/main/java/com/choicemaker/xmlencryption/StreamEncryptor.java
+++ src/main/java/com/choicemaker/xmlencryption/StreamEncryptor.java
@@ -16,137 +16,112 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.crypto.SecretKey;
-import javax.xml.stream.util.XMLEventAllocator;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
-import org.apache.xml.security.encryption.EncryptedData;
-import org.apache.xml.security.encryption.XMLCipher;
-import org.apache.xml.security.encryption.XMLEncryptionException;
-import org.apache.xml.security.keys.KeyInfo;
+import org.apache.xml.security.stax.ext.OutboundXMLSec;
+import org.apache.xml.security.stax.ext.SecurePart;
+import org.apache.xml.security.stax.ext.XMLSec;
 import org.apache.xml.security.stax.ext.XMLSecurityConstants;
-import org.apache.xml.security.stax.ext.XMLSecurityConstants.Action;
 import org.apache.xml.security.stax.ext.XMLSecurityProperties;
-import org.apache.xml.security.stax.impl.util.IDGenerator;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+//import org.apache.xml.security.test.stax.utils.XMLSecEventAllocator;
 
 import com.choicemaker.utilcopy01.KeyUtils;
 import com.choicemaker.utilcopy01.Precondition;
-import com.choicemaker.utilcopy01.SystemPropertyUtils;
-import com.choicemaker.utilcopy01.WSS4JConstants;
 
 public class StreamEncryptor {
 
-	private static final Logger logger =
-		Logger.getLogger(StreamEncryptor.class.getName());
+	// private static final Logger logger =
+	// Logger.getLogger(StreamEncryptor.class.getName());
 
 	static {
 		org.apache.xml.security.Init.init();
 	}
 
-	private static void encryptElement(Document doc, Element elementToEncrypt,
-			String docEncAlgo, SecretKey secretKey, KeyInfo keyInfo)
-			throws XMLEncryptionException {
-		final XMLCipher xmlCipher = XMLCipher.getInstance(docEncAlgo);
-		xmlCipher.init(XMLCipher.ENCRYPT_MODE, secretKey);
-		EncryptedData encData = xmlCipher.getEncryptedData();
-		final String xencEncryptedDataId = generateEncryptedDataId();
-		encData.setId(xencEncryptedDataId);
-		encData.setKeyInfo(keyInfo);
-		final boolean content = true;
-		logger.fine(
-				"Before encryption: " + SystemPropertyUtils.PV_LINE_SEPARATOR
-						+ XMLPrettyPrint.print(elementToEncrypt));
-		try {
-			xmlCipher.doFinal(doc, elementToEncrypt, content);
-		} catch (XMLEncryptionException e) {
-			logger.severe(e.toString());
-			throw e;
-		} catch (Exception e) {
-			logger.severe(e.toString());
-			String msg = "Failed to encrypt element '" + elementToEncrypt
-					+ "': " + e.getClass().getSimpleName();
-			throw new XMLEncryptionException(msg, e);
-		}
-		logger.fine("After encryption: " + SystemPropertyUtils.PV_LINE_SEPARATOR
-				+ XMLPrettyPrint.print(elementToEncrypt));
-	}
+	// private static void encryptElement(Document doc, Element
+	// elementToEncrypt,
+	// String docEncAlgo, SecretKey secretKey, KeyInfo keyInfo)
+	// throws XMLEncryptionException {
+	// final XMLCipher xmlCipher = XMLCipher.getInstance(docEncAlgo);
+	// xmlCipher.init(XMLCipher.ENCRYPT_MODE, secretKey);
+	// EncryptedData encData = xmlCipher.getEncryptedData();
+	// final String xencEncryptedDataId = generateEncryptedDataId();
+	// encData.setId(xencEncryptedDataId);
+	// encData.setKeyInfo(keyInfo);
+	// final boolean content = true;
+	// logger.fine(
+	// "Before encryption: " + SystemPropertyUtils.PV_LINE_SEPARATOR
+	// + XMLPrettyPrint.print(elementToEncrypt));
+	// try {
+	// xmlCipher.doFinal(doc, elementToEncrypt, content);
+	// } catch (XMLEncryptionException e) {
+	// logger.severe(e.toString());
+	// throw e;
+	// } catch (Exception e) {
+	// logger.severe(e.toString());
+	// String msg = "Failed to encrypt element '" + elementToEncrypt
+	// + "': " + e.getClass().getSimpleName();
+	// throw new XMLEncryptionException(msg, e);
+	// }
+	// logger.fine("After encryption: " + SystemPropertyUtils.PV_LINE_SEPARATOR
+	// + XMLPrettyPrint.print(elementToEncrypt));
+	// }
 
-	private static String generateEncryptedDataId() {
-		String retVal = IDGenerator.generateID("ED-");
-		return retVal;
-	}
+	// private static String generateEncryptedDataId() {
+	// String retVal = IDGenerator.generateID("ED-");
+	// return retVal;
+	// }
 
-	private static Element getDocumentElement(Document doc) {
-		Element retVal = doc.getDocumentElement();
-		if (retVal == null) {
-			NodeList nl = doc.getChildNodes();
-			assert nl != null;
-			final int countDocChildNodes = nl == null ? 0 : nl.getLength();
-			if (countDocChildNodes == 0) {
-				String msg = "Document has no nodes";
-				throw new IllegalArgumentException(msg);
-			}
-			if (countDocChildNodes > 1) {
-				String msg =
-					"Document has multiple child nodes: " + countDocChildNodes;
-				throw new IllegalArgumentException(msg);
-			}
-			Node n = nl.item(0);
-			if (!(n instanceof Element)) {
-				String msg = "Child node of document is not an element: '"
-						+ n.getClass().getName() + "'";
-				throw new IllegalArgumentException(msg);
-			}
-			retVal = (Element) n;
-		}
-		assert retVal != null;
-		return retVal;
-	}
+//	private final CredentialSet credential;
+//	private final EncryptionScheme scheme;
+//	private final SecretKeyInfoFactory skiFactory;
+	// private final EncryptedKeyFactory ekFactory = new EncryptedKeyFactory();
+	
+	private StreamEncryptor() {}
 
-	private final CredentialSet credential;
-	private final EncryptionScheme scheme;
-	private final SecretKeyInfoFactory skiFactory;
-	private final EncryptedKeyFactory ekFactory = new EncryptedKeyFactory();
+//	public StreamEncryptor(EncryptionScheme es, CredentialSet cs) {
+//		Precondition.assertNonNullArgument("null credentials", cs);
+//		Precondition.assertNonNullArgument("null scheme", es);
+//
+//		credential = cs;
+//		scheme = es;
+//		skiFactory = getSecretKeyInfoFactory(es, cs);
+//	}
 
-	public StreamEncryptor(EncryptionScheme es, CredentialSet cs) {
-		Precondition.assertNonNullArgument("null credentials", cs);
-		Precondition.assertNonNullArgument("null scheme", es);
-
-		credential = cs;
-		scheme = es;
-		skiFactory = es.getSecretKeyInfoFactory(cs,
+	public static SecretKeyInfoFactory getSecretKeyInfoFactory(
+			EncryptionScheme es, CredentialSet cs) {
+		SecretKeyInfoFactory skiFactory = es.getSecretKeyInfoFactory(cs,
 				es.getKeyEncryptionAlgorithm(), Collections.emptyMap());
+		return skiFactory;
 	}
 
-	public CredentialSet getCredential() {
-		return credential;
-	}
+//	public CredentialSet getCredential() {
+//		return credential;
+//	}
+//
+//	public EncryptionScheme getScheme() {
+//		return scheme;
+//	}
 
-	public EncryptionScheme getScheme() {
-		return scheme;
-	}
+	// private KeyInfo createKeyInfo(Document document, Element encKey) {
+	//
+	// KeyInfo keyInfo = new KeyInfo(document);
+	// keyInfo.addUnknownElement(encKey);
+	// Element keyInfoElement = keyInfo.getElement();
+	// keyInfoElement.setAttributeNS(WSS4JConstants.XMLNS_NS,
+	// "xmlns:" + WSS4JConstants.SIG_PREFIX, WSS4JConstants.SIG_NS);
+	//
+	// return keyInfo;
+	// }
 
-	private KeyInfo createKeyInfo(Document document, Element encKey) {
-
-		KeyInfo keyInfo = new KeyInfo(document);
-		keyInfo.addUnknownElement(encKey);
-		Element keyInfoElement = keyInfo.getElement();
-		keyInfoElement.setAttributeNS(WSS4JConstants.XMLNS_NS,
-				"xmlns:" + WSS4JConstants.SIG_PREFIX, WSS4JConstants.SIG_NS);
-
-		return keyInfo;
-	}
-
-	public OutputStream encrypt(InputStream sourceDocument)
-			throws XMLEncryptionException {
-		return encrypt(sourceDocument, DefaultAlgorithms.DECLARED_KEY_ENCRYPTION,
-				DefaultAlgorithms.DEFAULT_DOC_ENCRYPT_ALGORITHM);
-	}
+//	public void encrypt(InputStream sourceDocument, OutputStream outputStream)
+//			throws Exception {
+//		encrypt(sourceDocument, DefaultAlgorithms.DECLARED_KEY_ENCRYPTION,
+//				DefaultAlgorithms.DEFAULT_DOC_ENCRYPT_ALGORITHM, outputStream);
+//	}
 
 	/**
 	 * Encrypts the content of the root element of an XML document.
@@ -166,8 +141,15 @@ public class StreamEncryptor {
 	 *            {@link WSConstants#AES_256 AES_256}.
 	 * @throws Exception
 	 */
-	public OutputStream encrypt(final InputStream sourceDocument, String keyEncAlgo,
-			String docEncAlgo) throws XMLEncryptionException {
+//	public void encrypt(final InputStream sourceDocument, String keyEncAlgo,
+//			String docEncAlgo, OutputStream outputStream) throws Exception {
+//		encrypt(sourceDocument, keyEncAlgo, docEncAlgo, outputStream,
+//				skiFactory);
+//	}
+
+	public static void encrypt(final InputStream sourceDocument,
+			String keyEncAlgo, String docEncAlgo, OutputStream outputStream,
+			SecretKeyInfo ski) throws Exception {
 
 		// Preconditions
 		Precondition.assertNonNullArgument("null document", sourceDocument);
@@ -176,26 +158,43 @@ public class StreamEncryptor {
 		Precondition.assertNonEmptyString(
 				"null or blank document encryption algorithm", docEncAlgo);
 
-		throw new Error("Not yet implemented");
-//		// Find the document's root element and construct an XMLCipher instance
-//		final Element root = getDocumentElement(sourceDocument);
-//
-//		// Create the SecretKey that will encrypt the document
-//		SecretKeyInfo ski = skiFactory.createSessionKey();
-//		final SecretKey secretKey =
-//			KeyUtils.prepareSecretKey(docEncAlgo, ski.getKey());
-//
-//		// Create the encrypted key element that will replace the root content
-//		Element ek = ekFactory.createEncryptedKeyElement(sourceDocument,
-//				keyEncAlgo, ski);
-//		final KeyInfo keyInfo = createKeyInfo(sourceDocument, ek);
-//
-//		// Encrypt the content of the root element
-//		encryptElement(sourceDocument, root, docEncAlgo, secretKey, keyInfo);
-	}
+		final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
 
-	public static XMLEventAllocator getEventAllocator() throws Exception {
-		return new XMLSecEventAllocator();
+		// final XMLEventAllocator eventAllocator = getEventAllocator();
+		// final XMLInputFactory xmlInputFactory1 =
+		// XMLInputFactory.newInstance();
+		// xmlInputFactory1.setEventAllocator(eventAllocator);
+
+		// Create the SecretKey that will encrypt the document
+//		SecretKeyInfo ski = skiFactory.createSessionKey();
+		final SecretKey secretKey =
+			KeyUtils.prepareSecretKey(docEncAlgo, ski.getKey());
+
+		// // Create the encrypted key element that will replace the root
+		// content
+		// Element ek = ekFactory.createEncryptedKeyElement(sourceDocument,
+		// keyEncAlgo, ski);
+		// final KeyInfo keyInfo = createKeyInfo(sourceDocument, ek);
+		//
+		// // Encrypt the content of the root element
+		// encryptElement(sourceDocument, root, docEncAlgo, secretKey, keyInfo);
+
+		final XMLSecurityProperties encryptProperties =
+			StreamEncryptor.getEncryptionSecurityProperies(secretKey);
+		SecurePart.Modifier modifier = SecurePart.Modifier.Element;
+		SecurePart securePart = new SecurePart("", modifier);
+		securePart.setSecureEntireRequest(true);
+		encryptProperties.addEncryptionPart(securePart);
+
+		// Encrypt
+		XMLStreamReader xmlStreamReader =
+			xmlInputFactory.createXMLStreamReader(sourceDocument);
+		final OutboundXMLSec outbound =
+			XMLSec.getOutboundXMLSec(encryptProperties);
+		XMLStreamWriter xmlStreamWriter =
+			outbound.processOutMessage(outputStream, "UTF-8");
+		XMLBorrowedUtils.writeAll(xmlStreamReader, xmlStreamWriter);
+		xmlStreamWriter.close();
 	}
 
 	public static XMLSecurityProperties getEncryptionSecurityProperies(
--- src/test/java/com/choicemaker/xmlencryption/StreamEncryptorDecryptorTest.java
+++ src/test/java/com/choicemaker/xmlencryption/StreamEncryptorDecryptorTest.java
@@ -33,11 +33,13 @@ import org.apache.xml.security.stax.ext.XMLSec;
 import org.apache.xml.security.stax.ext.XMLSecurityProperties;
 import org.apache.xml.security.utils.XMLUtils;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
 import com.choicemaker.utilcopy01.KeyUtils;
+import com.choicemaker.utilcopy01.SystemPropertyUtils;
 
 /**
  * These tests require {#link AwsKmsProperties a property file} that defines
@@ -48,15 +50,75 @@ import com.choicemaker.utilcopy01.KeyUtils;
  */
 public class StreamEncryptorDecryptorTest {
 
-	// private static final Logger logger =
-	// Logger.getLogger(StreamEncryptorDecryptorTest2.class.getName());
+	private static final String EOL = SystemPropertyUtils.PV_LINE_SEPARATOR;
 
 	public static final int BUFFER_SIZE = 1000;
 
 	static {
-		// Security.addProvider(new
-		// org.bouncycastle.jce.provider.BouncyCastleProvider());
 		org.apache.xml.security.Init.init();
+	}
+
+	@Test
+	public void testStreamEncryptor() throws Exception {
+		
+		final String TEST = "testStreamEncryptor";
+
+		for (Object[] td : TestData.getTestData()) {
+
+			Assert.assertTrue(td != null && td.length == 2);
+			final String docName = (String) td[0];
+			final QName docRoot = (QName) td[1];
+			Assert.assertTrue(checkNodes(docName, docRoot, 1));
+
+			final XMLInputFactory xmlInputFactory =
+				XMLInputFactory.newInstance();
+
+			final XMLOutputFactory xmlOutputFactory =
+				XMLOutputFactory.newInstance();
+
+			final AwsKmsEncryptionScheme es = getEcryptionScheme();
+			final AwsKmsCredentialSet cs = getCredentialSet();
+
+			// Encrypt
+			final String keyEncAlgo = DefaultAlgorithms.DECLARED_KEY_ENCRYPTION;
+			final String docEncAlgo = 
+					DefaultAlgorithms.DEFAULT_DOC_ENCRYPT_ALGORITHM;
+			InputStream is;
+			is = this.getClass().getClassLoader().getResourceAsStream(docName);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			SecretKeyInfoFactory skiFactory = StreamEncryptor.getSecretKeyInfoFactory(es, cs);
+			final SecretKeyInfo ski = skiFactory.createSessionKey();
+			StreamEncryptor.encrypt(is, keyEncAlgo, docEncAlgo, baos, ski);
+
+			// Check the encrypted bytes
+			byte[] encrypted = baos.toByteArray();
+			String strEncrypted = new String(encrypted, "UTF-8");
+			System.out.println(TEST + " Encrypted: " + strEncrypted + EOL);
+			Assert.assertTrue(checkNodes(encrypted, docRoot, 0));
+
+			// Decrypt
+			final SecretKey secretKey = createSecretKey(ski,docEncAlgo);
+			final XMLSecurityProperties decryptProperties =
+				StreamEncryptor.getDecryptionSecurityProperies(secretKey);
+			final InboundXMLSec inbound =
+				XMLSec.getInboundWSSec(decryptProperties);
+			is = new ByteArrayInputStream(encrypted);
+			XMLStreamReader xmlStreamReader;
+			xmlStreamReader = xmlInputFactory.createXMLStreamReader(is);
+			xmlStreamReader = inbound.processInMessage(xmlStreamReader);
+			baos = new ByteArrayOutputStream();
+			XMLStreamWriter xmlStreamWriter;
+			xmlStreamWriter = xmlOutputFactory.createXMLStreamWriter(baos);
+			XMLBorrowedUtils.writeAll(xmlStreamReader, xmlStreamWriter);
+			xmlStreamWriter.close();
+
+			// Check the decrypted bytes
+			byte[] decrypted = baos.toByteArray();
+			String strDecrypted = new String(decrypted, "UTF-8");
+			System.out.println(TEST + " Decrypted: " + strDecrypted + EOL);
+			Assert.assertTrue(checkNodes(decrypted, docRoot, 1));
+		}
+
 	}
 
 	@Test
@@ -72,7 +134,7 @@ public class StreamEncryptorDecryptorTest {
 			final XMLInputFactory xmlInputFactory0 =
 				XMLInputFactory.newInstance();
 
-			final XMLEventAllocator eventAllocator = StreamEncryptor.getEventAllocator();
+			final XMLEventAllocator eventAllocator = getEventAllocator();
 			final XMLInputFactory xmlInputFactory1 =
 				XMLInputFactory.newInstance();
 			xmlInputFactory1.setEventAllocator(eventAllocator);
@@ -80,7 +142,9 @@ public class StreamEncryptorDecryptorTest {
 			final XMLOutputFactory xmlOutputFactory =
 				XMLOutputFactory.newInstance();
 
-			final SecretKey secretKey = createSecretKey();
+			AwsKmsCredentialSet ec = getCredentialSet();
+			AwsKmsEncryptionScheme es = getEcryptionScheme();
+			final SecretKey secretKey = createSecretKey(es, ec);
 
 			final XMLSecurityProperties encryptProperties =
 				StreamEncryptor.getEncryptionSecurityProperies(secretKey);
@@ -112,7 +176,7 @@ public class StreamEncryptorDecryptorTest {
 			byte[] encrypted = baos.toByteArray();
 			Assert.assertTrue(checkNodes(encrypted, docRoot, 0));
 			String strEncrypted = new String(encrypted, "UTF-8");
-			System.out.println("Encrypted: " + strEncrypted);
+			System.out.println("Encrypted: " + strEncrypted + EOL);
 
 			// Decrypt
 			is = new ByteArrayInputStream(encrypted);
@@ -126,24 +190,190 @@ public class StreamEncryptorDecryptorTest {
 			byte[] decrypted = baos.toByteArray();
 			Assert.assertTrue(checkNodes(decrypted, docRoot, 1));
 			String strDecrypted = new String(decrypted, "UTF-8");
-			System.out.println("Decrypted: " + strDecrypted);
+			System.out.println("Decrypted: " + strDecrypted + EOL);
 		}
 
 	}
+	
+	@Test
+	public void testXMLInboundOutboundXMLSec2() throws Exception {
 
-	private SecretKey createSecretKey() throws IOException {
-		// Create the SecretKey that will encrypt the document
+		for (Object[] td : TestData.getTestData()) {
+
+			Assert.assertTrue(td != null && td.length == 2);
+			final String docName = (String) td[0];
+			final QName docRoot = (QName) td[1];
+			Assert.assertTrue(checkNodes(docName, docRoot, 1));
+
+			final XMLInputFactory xmlInputFactory0 =
+				XMLInputFactory.newInstance();
+
+			final XMLInputFactory xmlInputFactory1 =
+				XMLInputFactory.newInstance();
+
+			final XMLOutputFactory xmlOutputFactory =
+				XMLOutputFactory.newInstance();
+
+			AwsKmsCredentialSet ec = getCredentialSet();
+			AwsKmsEncryptionScheme es = getEcryptionScheme();
+			final SecretKey secretKey = createSecretKey(es, ec);
+
+			final XMLSecurityProperties encryptProperties =
+				StreamEncryptor.getEncryptionSecurityProperies(secretKey);
+			SecurePart.Modifier modifier = SecurePart.Modifier.Element;
+			SecurePart securePart = new SecurePart("", modifier);
+			securePart.setSecureEntireRequest(true);
+			encryptProperties.addEncryptionPart(securePart);
+
+			final XMLSecurityProperties decryptProperties =
+				StreamEncryptor.getDecryptionSecurityProperies(secretKey);
+
+			// Encrypt
+			InputStream isClear = this.getClass().getClassLoader().getResourceAsStream(docName);
+			XMLStreamReader xmlClearReader = xmlInputFactory0.createXMLStreamReader(isClear);
+			ByteArrayOutputStream baosCypher = new ByteArrayOutputStream();
+			final OutboundXMLSec outboundCypher =
+			XMLSec.getOutboundXMLSec(encryptProperties);
+			XMLStreamWriter xmlCypherWriter = outboundCypher.processOutMessage(baosCypher, "UTF-8");
+			XMLBorrowedUtils.writeAll(xmlClearReader, xmlCypherWriter);
+			xmlCypherWriter.close();
+			byte[] encrypted = baosCypher.toByteArray();
+			Assert.assertTrue(checkNodes(encrypted, docRoot, 0));
+			String strEncrypted = new String(encrypted, "UTF-8");
+			System.out.println("Encrypted: " + strEncrypted + EOL);
+
+			// Decrypt
+			InputStream isCypher = new ByteArrayInputStream(encrypted);
+			XMLStreamReader xmlStreamReaderCypher = xmlInputFactory1.createXMLStreamReader(isCypher);
+			final InboundXMLSec inboundClear =
+			XMLSec.getInboundWSSec(decryptProperties);
+			xmlStreamReaderCypher = inboundClear.processInMessage(xmlStreamReaderCypher);
+			ByteArrayOutputStream baosClear = new ByteArrayOutputStream();
+			XMLStreamWriter xmlStreamWriterClear = xmlOutputFactory.createXMLStreamWriter(baosClear);
+			XMLBorrowedUtils.writeAll(xmlStreamReaderCypher, xmlStreamWriterClear);
+			xmlStreamWriterClear.close();
+
+			byte[] decrypted = baosClear.toByteArray();
+			Assert.assertTrue(checkNodes(decrypted, docRoot, 1));
+			String strDecrypted = new String(decrypted, "UTF-8");
+			System.out.println("Decrypted: " + strDecrypted + EOL);
+		}
+
+	}
+	
+	// @Test
+	// public void testXMLInboundOutboundXMLSec3() throws Exception {
+	//
+	// for (Object[] td : TestData.getTestData()) {
+	//
+	// Assert.assertTrue(td != null && td.length == 2);
+	// final String docName = (String) td[0];
+	// final QName docRoot = (QName) td[1];
+	// Assert.assertTrue(checkNodes(docName, docRoot, 1));
+	//
+	// final XMLInputFactory xmlInputFactory =
+	// XMLInputFactory.newInstance();
+	//
+	// final XMLOutputFactory xmlOutputFactory =
+	// XMLOutputFactory.newInstance();
+	//
+	// AwsKmsCredentialSet ec = getCredentialSet();
+	// AwsKmsEncryptionScheme es = getEcryptionScheme();
+	// final SecretKey secretKey = createSecretKey(es, ec);
+	//
+	// final XMLSecurityProperties encryptProperties =
+	// StreamEncryptor.getEncryptionSecurityProperies(secretKey);
+	// SecurePart.Modifier modifier = SecurePart.Modifier.Element;
+	// SecurePart securePart = new SecurePart("", modifier);
+	// securePart.setSecureEntireRequest(true);
+	// encryptProperties.addEncryptionPart(securePart);
+	//
+	// final XMLSecurityProperties decryptProperties =
+	// StreamEncryptor.getDecryptionSecurityProperies(secretKey);
+	//
+	// // Encrypt using an XMLStreamReader
+	// InputStream isClear =
+	// this.getClass().getClassLoader().getResourceAsStream(docName);
+	// XMLStreamReader xmlClearReader =
+	// xmlInputFactory.createXMLStreamReader(isClear);
+	// final InboundXMLSec inboundCypher =
+	// XMLSec.getInboundWSSec(encryptProperties);
+	// XMLStreamReader xmlStreamReaderCypher =
+	// inboundCypher.processInMessage(xmlClearReader);
+	// ByteArrayOutputStream baosCypher = new ByteArrayOutputStream();
+	//// final OutboundXMLSec outboundCypher =
+	//// XMLSec.getOutboundXMLSec(encryptProperties);
+	//// XMLStreamWriter xmlCypherWriter =
+	// outboundCypher.processOutMessage(baosCypher, "UTF-8");
+	//// XMLBorrowedUtils.writeAll(xmlClearReader, xmlCypherWriter);
+	//// xmlCypherWriter.close();
+	// XMLStreamWriter xmlStreamWriterCypher =
+	// xmlOutputFactory.createXMLStreamWriter(baosCypher);
+	// XMLBorrowedUtils.writeAll(xmlStreamReaderCypher, xmlStreamWriterCypher);
+	// xmlStreamWriterCypher.close();
+	// byte[] encrypted = baosCypher.toByteArray();
+	// String strEncrypted = new String(encrypted, "UTF-8");
+	// System.out.println("Encrypted: " + strEncrypted + EOL);
+	// Assert.assertTrue(checkNodes(encrypted, docRoot, 0));
+	//
+	// // Decrypt using an XMLStreamWriter
+	// InputStream isCypher = new ByteArrayInputStream(encrypted);
+	// xmlStreamReaderCypher = xmlInputFactory.createXMLStreamReader(isCypher);
+	// ByteArrayOutputStream baosClear = new ByteArrayOutputStream();
+	//// XMLStreamWriter xmlStreamWriterClear =
+	// xmlOutputFactory.createXMLStreamWriter(baosClear);
+	//// XMLBorrowedUtils.writeAll(xmlStreamReaderCypher, xmlStreamWriterClear);
+	//// xmlStreamWriterClear.close();
+	// final OutboundXMLSec outboundClear =
+	// XMLSec.getOutboundXMLSec(decryptProperties);
+	// XMLStreamWriter xmlClearWriter =
+	// outboundClear.processOutMessage(baosClear, "UTF-8");
+	// XMLBorrowedUtils.writeAll(xmlStreamReaderCypher, xmlClearWriter);
+	// xmlClearWriter.close();
+	//
+	// byte[] decrypted = baosClear.toByteArray();
+	// Assert.assertTrue(checkNodes(decrypted, docRoot, 1));
+	// String strDecrypted = new String(decrypted, "UTF-8");
+	// System.out.println("Decrypted: " + strDecrypted + EOL);
+	// }
+	//
+	// }
+	
+	private AwsKmsCredentialSet getCredentialSet() throws IOException {
 		String credentialName = "alice";
-		AwsKmsEncryptionScheme encScheme = new AwsKmsEncryptionScheme();
-		AwsKmsCredentialSet encCredentials =
+		AwsKmsCredentialSet retVal =
 			new AwsKmsCredentialSet(credentialName);
-		SecretKeyInfoFactory skiFactory = encScheme.getSecretKeyInfoFactory(
-				encCredentials, encScheme.getKeyEncryptionAlgorithm(),
-				Collections.emptyMap());
+		return retVal;
+	}
+	
+	private AwsKmsEncryptionScheme getEcryptionScheme() {
+		AwsKmsEncryptionScheme retVal = new AwsKmsEncryptionScheme();
+		return retVal;
+	}
+
+	private static XMLEventAllocator getEventAllocator() throws Exception {
+		return new XMLSecEventAllocator();
+	}
+
+	private SecretKey createSecretKey(AwsKmsEncryptionScheme encScheme,
+			AwsKmsCredentialSet encCredentials) throws IOException {
+		SecretKeyInfoFactory skiFactory = createSKIFactory(encCredentials, encScheme);
 		SecretKeyInfo ski = skiFactory.createSessionKey();
 		String docEncAlgo = DefaultAlgorithms.DEFAULT_DOC_ENCRYPT_ALGORITHM;
+		return createSecretKey(ski,docEncAlgo);
+	}
+	
+	private SecretKey createSecretKey(SecretKeyInfo ski, String docEncAlgo) {
 		byte[] rawKey = ski.getKey();
 		final SecretKey retVal = KeyUtils.prepareSecretKey(docEncAlgo, rawKey);
+		return retVal;
+	}
+
+	private static SecretKeyInfoFactory createSKIFactory(AwsKmsCredentialSet encCredentials, AwsKmsEncryptionScheme encScheme) throws IOException {
+		// Create the SecretKey that will encrypt the document
+		SecretKeyInfoFactory retVal = encScheme.getSecretKeyInfoFactory(
+				encCredentials, encScheme.getKeyEncryptionAlgorithm(),
+				Collections.emptyMap());
 		return retVal;
 	}
 
