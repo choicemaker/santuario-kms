77b40481935ab1cd2518323fe92b253732fe41ec
Rick Hall <rphall@choicemaker.com>
2016-12-25 11:31:21 -0500

    Removed test of Inbound/Outbound XMLSec from StreamEncryptorDecryptorTest

--- src/test/java/com/choicemaker/xmlencryption/StreamEncryptorDecryptorTest.java
+++ src/test/java/com/choicemaker/xmlencryption/StreamEncryptorDecryptorTest.java
@@ -17,28 +17,15 @@ import java.io.InputStream;
 import java.util.Collections;
 import java.util.Properties;
 
-import javax.crypto.SecretKey;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import javax.xml.stream.util.XMLEventAllocator;
 
-import org.apache.xml.security.stax.ext.InboundXMLSec;
-import org.apache.xml.security.stax.ext.OutboundXMLSec;
-import org.apache.xml.security.stax.ext.SecurePart;
-import org.apache.xml.security.stax.ext.XMLSec;
-import org.apache.xml.security.stax.ext.XMLSecurityProperties;
 import org.apache.xml.security.utils.XMLUtils;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
-import com.choicemaker.utilcopy01.KeyUtils;
 import com.choicemaker.utilcopy01.SystemPropertyUtils;
 
 /**
@@ -107,79 +94,6 @@ public class StreamEncryptorDecryptorTest {
 
 	}
 
-	@Test
-	public void testXMLInboundOutboundXMLSec() throws Exception {
-
-		for (Object[] td : TestData.getTestData()) {
-
-			Assert.assertTrue(td != null && td.length == 2);
-			final String docName = (String) td[0];
-			final QName docRoot = (QName) td[1];
-			Assert.assertTrue(checkNodes(docName, docRoot, 1));
-
-			final XMLInputFactory xmlInputFactory0 =
-				XMLInputFactory.newInstance();
-
-			final XMLInputFactory xmlInputFactory1 =
-				XMLInputFactory.newInstance();
-
-			final XMLOutputFactory xmlOutputFactory =
-				XMLOutputFactory.newInstance();
-
-			AwsKmsCredentialSet ec = getCredentialSet();
-			AwsKmsEncryptionScheme es = getEcryptionScheme();
-			final SecretKey secretKey = createSecretKey(es, ec);
-
-			final XMLSecurityProperties encryptProperties =
-				StreamEncryptor.getEncryptionSecurityProperies(secretKey);
-			SecurePart.Modifier modifier = SecurePart.Modifier.Element;
-			SecurePart securePart = new SecurePart("", modifier);
-			securePart.setSecureEntireRequest(true);
-			encryptProperties.addEncryptionPart(securePart);
-
-			final XMLSecurityProperties decryptProperties =
-				StreamDecryptor.getDecryptionSecurityProperies(secretKey);
-
-			// Encrypt
-			InputStream isClear =
-				this.getClass().getClassLoader().getResourceAsStream(docName);
-			XMLStreamReader xmlClearReader =
-				xmlInputFactory0.createXMLStreamReader(isClear);
-			ByteArrayOutputStream baosCypher = new ByteArrayOutputStream();
-			final OutboundXMLSec outboundCypher =
-				XMLSec.getOutboundXMLSec(encryptProperties);
-			XMLStreamWriter xmlCypherWriter =
-				outboundCypher.processOutMessage(baosCypher, "UTF-8");
-			XMLBorrowedUtils.writeAll(xmlClearReader, xmlCypherWriter);
-			xmlCypherWriter.close();
-			byte[] encrypted = baosCypher.toByteArray();
-			Assert.assertTrue(checkNodes(encrypted, docRoot, 0));
-			String strEncrypted = new String(encrypted, "UTF-8");
-			System.out.println("Encrypted: " + strEncrypted + EOL);
-
-			// Decrypt
-			InputStream isCypher = new ByteArrayInputStream(encrypted);
-			XMLStreamReader xmlStreamReaderCypher =
-				xmlInputFactory1.createXMLStreamReader(isCypher);
-			final InboundXMLSec inboundClear =
-				XMLSec.getInboundWSSec(decryptProperties);
-			xmlStreamReaderCypher =
-				inboundClear.processInMessage(xmlStreamReaderCypher);
-			ByteArrayOutputStream baosClear = new ByteArrayOutputStream();
-			XMLStreamWriter xmlStreamWriterClear =
-				xmlOutputFactory.createXMLStreamWriter(baosClear);
-			XMLBorrowedUtils.writeAll(xmlStreamReaderCypher,
-					xmlStreamWriterClear);
-			xmlStreamWriterClear.close();
-
-			byte[] decrypted = baosClear.toByteArray();
-			Assert.assertTrue(checkNodes(decrypted, docRoot, 1));
-			String strDecrypted = new String(decrypted, "UTF-8");
-			System.out.println("Decrypted: " + strDecrypted + EOL);
-		}
-
-	}
-
 	private AwsKmsCredentialSet getCredentialSet() throws IOException {
 		String credentialName = "alice";
 		AwsKmsCredentialSet retVal = new AwsKmsCredentialSet(credentialName);
@@ -188,31 +102,6 @@ public class StreamEncryptorDecryptorTest {
 
 	private AwsKmsEncryptionScheme getEcryptionScheme() {
 		AwsKmsEncryptionScheme retVal = new AwsKmsEncryptionScheme();
-		return retVal;
-	}
-
-	private SecretKey createSecretKey(AwsKmsEncryptionScheme encScheme,
-			AwsKmsCredentialSet encCredentials) throws IOException {
-		SecretKeyInfoFactory skiFactory =
-			createSKIFactory(encCredentials, encScheme);
-		SecretKeyInfo ski = skiFactory.createSessionKey();
-		String docEncAlgo = DefaultAlgorithms.DEFAULT_DOC_ENCRYPT_ALGORITHM;
-		return createSecretKey(ski, docEncAlgo);
-	}
-
-	private SecretKey createSecretKey(SecretKeyInfo ski, String docEncAlgo) {
-		byte[] rawKey = ski.getKey();
-		final SecretKey retVal = KeyUtils.prepareSecretKey(docEncAlgo, rawKey);
-		return retVal;
-	}
-
-	private static SecretKeyInfoFactory createSKIFactory(
-			AwsKmsCredentialSet encCredentials,
-			AwsKmsEncryptionScheme encScheme) throws IOException {
-		// Create the SecretKey that will encrypt the document
-		SecretKeyInfoFactory retVal = encScheme.getSecretKeyInfoFactory(
-				encCredentials, encScheme.getKeyEncryptionAlgorithm(),
-				Collections.emptyMap());
 		return retVal;
 	}
 
